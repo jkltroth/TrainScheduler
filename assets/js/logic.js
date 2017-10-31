b623898459c785587803e05737b28698107ce024
James Roth <jkltroth@gmail.com>
2017-10-30 22:45:53 -0400

    Basic logic finished. Need to determine best way to calculate nextArrival

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -4,58 +4,61 @@
 
 // Initialize Firebase
 var config = {
-    apiKey: "AIzaSyAQutCgUBAPV6irOcIIe4nYU7Z4k9mm-bs",
-    authDomain: "billablehours-8d506.firebaseapp.com",
-    databaseURL: "https://billablehours-8d506.firebaseio.com",
-    projectId: "billablehours-8d506",
+    apiKey: "AIzaSyAUmkblUeqpPb1Wh0kgsMyuiPLBw83jn3M",
+    authDomain: "trainscheduler-986a9.firebaseapp.com",
+    databaseURL: "https://trainscheduler-986a9.firebaseio.com",
+    projectId: "trainscheduler-986a9",
     storageBucket: "",
-    messagingSenderId: "839388582447"
+    messagingSenderId: "861567155177"
 };
 firebase.initializeApp(config);
 
 // Create a variable to reference the database.
 var database = firebase.database();
 
+// On click function for the submit button..
 $('#submit-btn').on('click', function (event) {
+    // Prevent page refresh
     event.preventDefault();
 
-    var name = $("#employeeName").val().trim();
-    var role = $("#employeeRole").val().trim();
-    var rate = $("#monthlyRate").val().trim();
-    var startDate = $("#startDateInput").val().trim();
+    // Assign inputs from form to variables..
+    var trainName = $("#trainNameInput").val().trim();
+    var destination = $("#destinationInput").val().trim();
+    var trainTime = $("#trainTimeInput").val().trim();
+    var frequency = $("#frequencyInput").val().trim();
 
-    console.log(startDate);
-
+    // Push input values (as variables) to the database
     database.ref().push({
-        name: name,
-        role: role,
-        rate: rate,
-        startDate: startDate
+        trainName: trainName,
+        destination: destination,
+        trainTime: trainTime,
+        frequency: frequency
     });
 
 });
 
-// Firebase watcher + initial loader HINT: This code behaves similarly to .on("value")
+// Firebase watcher + initial loader
 database.ref().on("child_added", function (childSnapshot) {
 
     // Log everything that's coming out of snapshot
-    console.log(childSnapshot.val().name);
-    console.log(childSnapshot.val().role);
-    console.log(childSnapshot.val().rate);
-    console.log(childSnapshot.val().startDate);
+    console.log(childSnapshot.val().trainName);
+    console.log(childSnapshot.val().destination);
+    console.log(childSnapshot.val().trainTime);
+    console.log(childSnapshot.val().frequency);
 
-    var monthsWorked = moment().diff(moment((childSnapshot.val().startDate), "MM-DD-YYYY"), "months");
+    // var monthsWorked = moment().diff(moment((childSnapshot.val().startDate), "MM-DD-YYYY"), "months");
 
-    var totalBilled = monthsWorked * (childSnapshot.val().rate);
+    var currentTime = moment();
+
+    // var totalBilled = monthsWorked * (childSnapshot.val().rate);
 
     // full list of items to the well
-    $("#currentEmployees").append(
-        "<tr class='employee'><td id='name'> " + childSnapshot.val().name +
-        " </td><td id='role'> " + childSnapshot.val().role +
-        " </td><td id='startDateCol'> " + childSnapshot.val().startDate +
-        " </td><td id='monthsWorked'> " + monthsWorked +
-        " </td><td id='rate'> " + childSnapshot.val().rate +
-        " </td><td id='totalBilled'> " + totalBilled + " </td></tr>");
+    $("#currentTrains").append(
+        "<tr class='train'><td id='trainName'> " + childSnapshot.val().trainName +
+        " </td><td id='destination'> " + childSnapshot.val().destination +
+        " </td><td id='frequency'> " + childSnapshot.val().frequency +
+        " </td><td id='nextArrival'> " + "nextArrival" +
+        " </td><td id='minutesAway'> " + "minutesAway" + "</td></tr>");
 
     // Handle the errors
 }, function (errorObject) {
