3953488e9250a1eaabed1e54bd99cbda31147a82
James Roth <jkltroth@gmail.com>
2017-10-30 23:00:22 -0400

    Pseudocoded nextArrival and minutesAway logic

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -24,14 +24,14 @@ $('#submit-btn').on('click', function (event) {
     // Assign inputs from form to variables..
     var trainName = $("#trainNameInput").val().trim();
     var destination = $("#destinationInput").val().trim();
-    var trainTime = $("#trainTimeInput").val().trim();
+    var trainStartTime = $("#trainStartTimeInput").val().trim();
     var frequency = $("#frequencyInput").val().trim();
 
     // Push input values (as variables) to the database
     database.ref().push({
         trainName: trainName,
         destination: destination,
-        trainTime: trainTime,
+        trainStartTime: trainStartTime,
         frequency: frequency
     });
 
@@ -43,14 +43,23 @@ database.ref().on("child_added", function (childSnapshot) {
     // Log everything that's coming out of snapshot
     console.log(childSnapshot.val().trainName);
     console.log(childSnapshot.val().destination);
-    console.log(childSnapshot.val().trainTime);
+    console.log(childSnapshot.val().trainStartTime);
     console.log(childSnapshot.val().frequency);
 
     // var monthsWorked = moment().diff(moment((childSnapshot.val().startDate), "MM-DD-YYYY"), "months");
+    // var totalBilled = monthsWorked * (childSnapshot.val().rate);
 
+    // Set current time to variable
     var currentTime = moment();
 
-    // var totalBilled = monthsWorked * (childSnapshot.val().rate);
+    //Set nextArrival var to trainStartTime.
+    var nextArrival = childSnapshot.val().trainStartTime;
+
+    // While nextArrival < currentTime, then: nextArrival = nextArrival + frequency
+    // Once nextArrival >= currentTime, break from loop. 
+
+    //var minutesAway = minutes from currentTime until nextArrival
+
 
     // full list of items to the well
     $("#currentTrains").append(
